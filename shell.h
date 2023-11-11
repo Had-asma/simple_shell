4beb5fbd2cded20c1657fd616b6fd7421958752e
AsmaHad <asmahad25@gmail.com>
2023-11-11 03:24:32 -0800

    a

---
+++ shell.h
@@ -0,0 +1,44 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcnt1.h>
+
+#define DELIM " \t\n"
+
+extern char **environ;
+
+int _execute(char **command, char **argv, int idx);
+void freecmd(char **arr);
+
+char *read_line(void);
+char **tokenizer(char *line);
+
+char *_strdup(const char *str);
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+
+char *_getenv(char *var);
+char *_getpath(char *command);
+
+char *_inoa(int n);
+void reverse_string(char *dtr, int len);
+void print_err(char *name, char *cmd, int idx);
+
+void handle_builtin(char **command, char **argv, int *status, int idx);
+int is_builtin(char *command);
+void exit_shell(char **command, char **argv, int *status, int idx);
+void print_env(char **command, int *status);
+
+int is_positive_num(char *str);
+int _atoi(char *str);
+
+#endif
