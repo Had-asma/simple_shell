ed66b995af8630096a5af76b2103a5f93e118759
AsmaHad <asmahad25@gmail.com>
2023-11-12 02:36:37 -0800

    a

---
+++ touls.c
@@ -0,0 +1,110 @@
+#include "shell.h"
+/**
+ * _itoa - ....
+ * @n : integer
+ * Return : string
+ */
+
+char *_itoa(int n)
+{
+	char buffer[20];
+	int i = 0;
+
+	if (n == 0)
+		buffer[i++] = (n % 10) + '0';
+	else
+	{
+		while (n > 0)
+		{
+			buffer[i++] = (n % 10) + '0';
+			n /= 10;
+		}
+	}
+	buffer[i] = '\0';
+	reverse_string(buffer, i);
+
+	return (_strdup(buffer));
+}
+/**
+ * reverse_string - reverse strings
+ * @len : integer
+ * @str : ...
+ * Return : string
+ */
+
+void reverse_string(char *str, int len)
+{
+	char tmp;
+	int start = 0;
+	int end = len - 1;
+
+	while (start < end)
+	{
+		tmp = str[start];
+		str[start] = str[end];
+		str[end] = tmp;
+		start++;
+		end--;
+	}
+}
+
+/**
+ * print_err - output of error message
+ * @name : name of command
+ * @cmd : comma,d 
+ * @idx : index of line
+ * Return : string
+ */
+
+void print_err(char *name, char *cmd, int idx)
+{
+	char *index, mssg[] = ": not found\n";
+
+	index = _itoa(idx);
+
+	write(STDERR_FILENO, name, _strlen(name));
+	write(STDERR_FILENo, ": ", 2);
+	write(STDERR_FILENO, index, _strlen(index));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, cmd, _strlen(cmd));
+	write(STDERR_FILENO, mssg, _strlen(mssg));
+	free(index);
+}
+
+/**
+ * is_positive_num - ...
+ * @str : ....
+ * Return : integer
+ */
+
+int is_positive_num(char *str)
+{
+	int i ;
+
+	if (!str)
+		return (0);
+	for (i = 0, str[i]; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * _atoi - ...
+ * @str : ...;
+ * Retuen : integer
+ */
+
+int _atoi(char *str)
+{
+	int i, num = 0;
+
+	for (i = 0; str[i]; i++)
+	{
+		num *= 10;
+		num += (str[i] - '0');
+	}
+	return (num);
+}
