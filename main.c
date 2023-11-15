2548a2ed579ad15ce91f73006865e29718f832ff
AsmaHad <asmahad25@gmail.com>
2023-11-15 03:27:20 -0800

    aa

---
+++ main.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+
+/**
+ * main - simple shell function
+ * @ac : count of arguments
+ * @argv : arguments
+ * Return : 0 always.
+ */
+
+int main(int ac, char **argv)
+{
+	char *line = NULL;
+	char **cmd = NULL;
+	int status = 0, idx = 0;
+	(void) ac;
+	while (1)
+	{
+		line = read_line();
+		if (line == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			return (status);
+		}
+
+		idx++;
+		cmd = tokenizer(line);
+		if (!cmd)
+			continue;
+		if (is_builtin(cmd[0]))
+			handl_builtin(cmd, argv, &status, idx);
+		else
+			status = _execute(cmd, argv, idx);
+	}
+}
--- shell.h
+++ shell.h
@@ -8,7 +8,7 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <errno.h>
-#include <fcnt1.h>
+#include <fcntl.h>
 
 #define DELIM " \t\n"
 
